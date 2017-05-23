1b4e85e5a98b86ba90800dd84d79d3c2412c01c4
Joseph Harrison <whiplashomega@gmail.com>
2017-05-23 13:31:31 +0000

    attempting to fix mongodb connection issues

--- config/db.js
+++ config/db.js
@@ -1,3 +1,9 @@
     module.exports = {
-        url : 'mongodb://localhost/stencil-dev'
+        url : (function() {
+            if(process.env.PORT == 8080) {
+                return 'mongodb://localhost/stencil-dev';
+            } else {
+                return 'mongodb://whiplashomega:betagamma#2@ds149511.mlab.com:49511/heroku_5c4snn97';
+            }
+        })()
     }
