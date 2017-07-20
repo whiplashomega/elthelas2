2905e36dd6ced0663bd064caa516137aca13c7d9
Joseph Harrison <whiplashomega@gmail.com>
2017-07-20 19:59:03 +0000

    browse gods by alignment or domain

--- dev/js/controllers/GodsController.js
+++ dev/js/controllers/GodsController.js
@@ -6,11 +6,27 @@
     $scope.gods = false;
     $scope.GodByName = "";
     $scope.alignment = "";
+    $scope.domain = "";
+    $scope.alignmentGods = [];
+    $scope.domainGods = [];
     
     $scope.loadGod = function(id) {
       $scope.GodByName = $scope.gods.filter(function(god) {
         return god.id === id;
       })[0];
+    };
+    
+    $scope.alignmentSelect = function() {
+      $scope.alignmentGods = $scope.gods.filter(function(god) {
+        return god.alignment === $scope.alignment;
+      });
+    };
+    
+    $scope.domainSelect = function() {
+      $scope.domainGods = $scope.gods.filter(function(god) {
+        var domains = god.domains5.split(", ");
+        return domains.includes($scope.domain);
+      });
     };
     
     godProvider.getGods().then(function(response) {
