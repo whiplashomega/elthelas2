a6ad7785a938106bd588c1ca1631ee15d667cbec
Joseph Harrison <whiplashomega@gmail.com>
2017-08-09 13:30:33 +0000

    removed redundant config file

--- config/db.js
+++
@@ -1,9 +0,0 @@
-    module.exports = {
-        url : (function() {
-            if(process.env.PORT == 8080) {
-                return 'mongodb://localhost/stencil-dev';
-            } else {
-                return 'mongodb://whiplashomega:betagamma#2@ds149511.mlab.com:49511/heroku_5c4snn97';
-            }
-        })()
-    }
