26824ac8c691846786fca03626b061b38d057e05
Joe Harrison <whiplashomega@gmail.com>
2018-07-26 18:22:14 +0000

    yep

--- elthelasapp/src/components/header/headercomponent.vue
+++ elthelasapp/src/components/header/headercomponent.vue
@@ -18,7 +18,7 @@
                 <li class="dropdown-item" v-if="loggedin"><a ui-sref="app.book1">Epic of Elthelas Book 1</a></li>
                 <li class="dropdown-item" v-if="loggedin"><a href="#">Epic of Elthelas Book 2</a></li>
                 <li class="dropdown-item" v-if="loggedin"><a href="#">Epic of Elthelas Book 3</a></li>
-                <li class="dropdown-item" v-if="loggedin && (userinfo.username === 'whiplashomega' || userinfo.username === 'Failbot7000')"><router-link to="/tools/book4/dm/curestan">Epic of Elthelas Book 4</router-link></li>
+                <li class="dropdown-item" v-if="loggedin && (userinfo.username === 'whiplashomega')"><router-link to="/tools/book4/dm/curestan">Epic of Elthelas Book 4</router-link></li>
                 <li class="dropdown-item"><router-link to="/tools/book4/playersguide">Epic of Elthelas Book 4 - Player's Guide</router-link></li>
               </ul>
           </div>
