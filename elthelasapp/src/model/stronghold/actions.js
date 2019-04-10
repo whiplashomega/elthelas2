830453cf21dbde3de99f05cd651a77e0c37a5d31
Joe Harrison <whiplashomega@gmail.com>
2019-04-10 16:02:00 +0000

    more fixes

--- elthelasapp/src/model/stronghold/actions.js
+++ elthelasapp/src/model/stronghold/actions.js
@@ -1,32 +1,31 @@
 import Vue from 'vue';
-import ctest from '../cordovatest.js';
 import Stronghold from '../classes/stronghold';
 
 export default {
   getAllImprovements ({ commit }) {
     return new Promise((resolve) => {
-      Vue.http.get(ctest.baseUrl + 'json/improvements.json').then((response) => {
+      Vue.http.get('/json/improvements.json').then((response) => {
         var improvements = response.body.documents;
         commit('GET_IMPROVEMENTS', { improvements: improvements });
         resolve();
+        return true;
       });
     });
   },
   newStronghold: ({ state }) => {
     state.current = new Stronghold();
   },
-  getAllStrongholds: ({ state, getters }) => {
-    return new Promise((resolve) => {
-      Vue.http.get('/strongholds').then((res) => {
-        state.strongholds = res.body;
-        resolve();
-      });
+  getAllStrongholds: ({ state }) => {
+    return Vue.http.get('/strongholds').then((res) => {
+      state.strongholds = res.body;
+      return true;
     });
   },
   saveNewStronghold: ({ state, getters }) => {
     Vue.http.post('/strongholds?token=' + getters.getUserInfo.token, { stronghold: { ...state.current, _id: undefined } }).then((res) => {
       state.current = { ...res.body };
       state.strongholds.push(res.body);
+      return true;
     });
   },
   loadStronghold: ({ state }, stronghold) => {
@@ -52,6 +51,10 @@ export default {
     if (check) {
       Vue.http.delete('/strongholds/' + stronghold._id + "?token=" + getters.getUserInfo.token).then(() => {
         state.strongholds.splice(state.all.indexOf(stronghold));
+        return true;
+      }).catch(() => {
+        alert("failed to delete stronghold");
+        return false;
       });
     }
   }
