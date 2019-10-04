041017ab8cf77645778c54f9754aa8742eef1b33
Joe Harrison <whiplashomega@gmail.com>
2019-10-04 20:40:00 +0000

    add a bunch of spells I somehow missed my first time through XGE

--- elthelasapp/dist/service-worker.js
+++ elthelasapp/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.447f5211053733982bff8321b1081866.js"
+  "/precache-manifest.3795e4cf7bc0a983ed324b9b6e6061a4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Elthelas"});
