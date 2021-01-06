52ea519fce6921dcea0d608896f25a82c33d7ef9
Joe Harrison <whiplashomega@gmail.com>
2021-01-06 23:53:36 +0000

    yep

--- elthelasapp/src/components/history/historylist.vue
+++ elthelasapp/src/components/history/historylist.vue
@@ -4,7 +4,9 @@
     <div>
       <div v-for="event in sortedEvents" :key="event.start_date.year + event.start_date.month + event.start_date.day + event.text.headline">
         <h3>{{ event.text.headline }}</h3>
-        <h4>{{ event.start_date.day }} {{ event.start_date.month }}, {{ event.start_date.year }} - {{ event.end_date.day }} {{ event.end_date.month }}, {{ event.end_date.year }}</h4>
+        <h4>
+          <span v-if="event.start_date.month">{{ event.start_date.day }} {{ event.start_date.month }},</span>
+          {{ event.start_date.year }}<span v-if="event.end_date.year"> - <span v-if="event.end_date.month">{{ event.end_date.day }} {{ event.end_date.month }},</span> {{ event.end_date.year }}</span></h4>
         <div v-html="$options.filters.marked(event.text.text)"></div>
       </div>
     </div>
